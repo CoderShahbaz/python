590e329d64ee945df7b82ac19bb0ccff1b6906ae
shahbazthestudent <shahshahbaz5@gmail.com>
2024-03-05 14:21:20 -0800

    JS and Project1

---
+++ JS/Day16/script.js
@@ -0,0 +1,34 @@
+/*Area of Shapes*/
+
+/*Area of Triangle*/
+
+var base=25
+var height=10
+
+var areaTriangle=1/2*base*height
+console.log("Area of Triangle: "+areaTriangle );
+
+
+/*Area of Cricle*/
+
+var radius=15
+var pi=3.14
+
+var areaCircle=pi*radius*radius
+console.log("Area of Circle: "+areaCircle );
+
+/*Area of Rectangle*/
+
+var lenght=25
+var width=12
+var areaRectangle=lenght*width
+
+console.log("Area of Rectangle: "+areaRectangle );
+
+
+/*Area of a Square*/
+
+ var length=13
+ var areaSquare=lenght*length
+
+ console.log("Area of Square "+areaSquare );
